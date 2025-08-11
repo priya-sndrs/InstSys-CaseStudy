9baf7e5ff83a45e8660463d8c20b970070d49a02
乃ЕTA <TA>
2025-08-12 00:16:13 +0800

    initial commit

--- backend/Conn.py
+++ backend/Conn.py
@@ -3,6 +3,12 @@ from chromadb.config import Settings
 
 client = chromadb.Client(Settings())
 
-
-def Connection():
-  return
+def Server(chroma_db_impl, persist_directory):
+  client = chromadb.Client(Settings(
+    chroma_db_impl=f"{chroma_db_impl}",
+    persist_directory=f"{persist_directory}"
+  ))
+  
+def GetUserCollection(user_id: str):
+  return client.get_or_create_collection(name=f"user_{user_id}")
+  
--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,51 @@
-from flask import Flask, jsonify
+from Conn import Server, GetUserCollection
+from flask import Flask, jsonify, request
+from datetime import datetime
 
 app = Flask(__name__)
+chromadb_db_impl = "duckdb+parquet"
+persist_directory = "./chroma_db"
+Server(chromadb_db_impl, persist_directory)
 
 @app.route('/')
 def Login():
-  return jsonify('login successful')
+  return jsonify('login successful')  
 
-def Connection():
-  return
+@app.route('/register', methods=['POST'])
+def RegisterUserAccount():
+  data = request.get_json()
+  
+  username = data.get('Username')
+  password = data.get('Password')
+  user_id = data.get('School_id')
+  email = data.get('Email')
+  
+  if not username or not password or not user_id or not email:
+    return jsonify({"error": "Missing required field username, password, user_id, email"}), 400
+@app.route('/Get_LLM_response')
+def GetUserAndLLMResponse():
+  data = request.get_json()
+  if data is None:
+    jsonify({'error': 'Missing Data'}), 400
+  user_id = data.get('user_id')
+  user_query = data.get('user_query')
+  llm_response = data.get('llm_response')
+  
+  if not user_id or not user_query or not llm_response:
+    return jsonify({'error': 'Mising required fields'}), 400
+  
+  collection = GetUserCollection(user_id)
+  collection.add(
+        documents=[llm_response],
+        metadatas=[{
+            "user_id": user_id,
+            "query": user_query,
+            "timestamp": datetime.now().isoformat()
+        }],
+        ids=[f"{user_id}_{datetime.now().timestamp()}"]
+  )
+  
+  return jsonify({'status': 'stored'})
 
 if __name__ == "__main__":
   app.run()
