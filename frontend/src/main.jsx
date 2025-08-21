af716cc0fb71f9daa66fa4ef97b738583d84c3d0
priya-sndrs <sanandresprincessapril@gmail.com>
2025-08-21 22:18:22 +0800

    dashboard
    - profile-circle.png
    - dashboard.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import ChatPrompt from './chatPrompt.jsx'
 import Navigate from './navigate.jsx'
+import Dashboard from './dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navigate /> 
+    <Dashboard /> 
   </StrictMode>,
 )
